12cdb6ee3140b343e72c99903e6a998288522375
cianmcder <cianmcder@gmail.com>
2017-05-09 18:56:39 -0700

    Update MyHttpServer.java

--- src/main/java/com/csumb/cst438/a1/MyHttpServer.java
+++ src/main/java/com/csumb/cst438/a1/MyHttpServer.java
@@ -41,8 +41,9 @@ public class MyHttpServer {
 			String uri = t.getRequestURI().toString();
 			String u, letters = "abcdefghijklmnopqrstuvwxyz";
 			boolean flag = false;
-			//Makes it so user doesn't worry about upper or lowercase, and sheds "/?guess=" from uri
+			//Makes it so user doesn't worry about upper or lowercase, and sheds "/" and "?guess=" from uri
 			u = uri.toLowerCase();
+			u = u.replace("/", "");
 			u = u.replace("?guess", "");
 			//Checks if the user properly input a letter. Also takes into account when URI has a .gif to prevent error in this test
 			if ((u.length() == 0 || u.length() > 1 || letters.contains(u) == false) && uri.contains(".gif") == false) {
